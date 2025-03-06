92bff33dbdcdb23bbedb05834183fca10e645696
Hy-LeTuan <letuanhy1507@gmail.com>
2025-03-06 12:11:56 -0600

    minor update

--- src/server/main.c
+++ src/server/main.c
@@ -6,77 +6,50 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
 
-#define MYPORT "1200"
-#define BACKLOG 10
-
-void client() {
-    int status;
+int main(int arc, char* argv[]) {
     struct addrinfo hints;
     struct addrinfo* servinfo;
-    struct addrinfo* selfinfo;
+    struct addrinfo* p;
+    int status;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    int signal = getaddrinfo(NULL, "1200", &hints, &servinfo);
-
-    if (getaddrinfo(NULL, "1200", &hints, &servinfo) == 0 &&
-        getaddrinfo(NULL, "1300", &hints, &selfinfo) == 0) {
-        int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype,
-                            servinfo->ai_protocol); // configure local port
-        bind(sockfd, selfinfo->ai_addr, selfinfo->ai_addrlen);
-
-        if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
-            printf("connection failed\n");
-            exit(-1);
-        }
-        printf("connection established");
-    }
-}
-
-int main() {
-    int status;
-    struct addrinfo hints;
-    struct addrinfo* servinfo;
-    int sockfd;
-
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
-    int new_fd;
-    char buffer;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // this is your own IP address
-
-    status = getaddrinfo(NULL, MYPORT, &hints, &servinfo);
-
-    if (status == 0) {
-        sockfd = socket(servinfo->ai_family, servinfo->ai_socktype,
-                        servinfo->ai_protocol);
-        int opt = 1;
-        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof opt);
-        bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
-        listen(sockfd, BACKLOG); // listen on port 1200;
-
-        addr_size = sizeof their_addr;
-        printf("reached this line\n");
-        new_fd = accept(sockfd, (struct sockaddr*)&their_addr,
-                        &addr_size); // awaiting new connections
-        printf("reached new line\n");
-
-        if (new_fd == -1) {
-            printf("connection failed\n");
-        } else {
-            printf("connection succeeded\n");
-        }
-    } else {
+    // get address info of the IP address specified in parameter
+    status = getaddrinfo(argv[1], NULL, &hints, &servinfo);
+    if (status != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
     }
+
+    // since it's a linked list, loop through the nodes
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        // get the IP address in the struct sockaddr to easily access the
+        // properties of this socket
+
+        char addr_string[INET6_ADDRSTRLEN];
+        void* addr;
+        char* ipver;
+
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+            ipver = "IPv4";
+            addr = &(ipv4->sin_addr);
+            // inet_ntop(AF_INET, addr, addr_string, INET_ADDRSTRLEN);
+        } else if (p->ai_family == AF_INET6) {
+            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
+            ipver = "IPv6";
+            addr = &(ipv6->sin6_addr);
+            // inet_ntop(AF_INET6, addr, addr_string, INET6_ADDRSTRLEN);
+        }
+
+        // or we can get the address out here and just use the family as signal
+        inet_ntop(p->ai_family, addr, addr_string, sizeof addr_string);
+        printf("protocol: %s with address: %s\n", ipver, addr_string);
+    }
+
+    freeaddrinfo(servinfo);
 }
